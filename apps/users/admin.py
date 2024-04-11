5e7fedef15df522f56c0fd0fea0da331c2be2aa2
Victor Tetteh-Ganoh Ahumah <kofiganoh@gmail.com>
2024-04-11 21:36:28 +0000

    add custom user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from apps.users.models import User
+from django.utils.translation import gettext_lazy as _
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ("phone_number", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "phone_number",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+    list_display = ("id", "phone_number", "first_name", "last_name", "is_staff",)
+    ordering = ("phone_number",)
