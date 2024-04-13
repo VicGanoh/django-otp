b8e41d0b737dec720089d2261676c175c999f201
Victor Tetteh-Ganoh Ahumah <kofiganoh@gmail.com>
2024-04-13 11:06:52 +0000

    update managers.py

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -1,37 +1,35 @@
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import UserManager
+from django.contrib.auth.hashers import make_password
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(UserManager):
+    """Custom manager for the User model."""
+
+    def _create_user(self, phone_number, password=None, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not phone_number:
+            msg = "The given phone number must be set"
+            raise ValueError(msg)
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
     def create_user(self, phone_number, password=None, **extra_fields):
-        """
-        Creates and saves a User with the given phone_number and password.
-        """
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
+        return self._create_user(phone_number, password, **extra_fields)
 
-        if not phone_number:
-            raise ValueError("Phone number field is required")
-
-        user = self.model(
-            phone_number=phone_number,
-            **extra_fields,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
     def create_superuser(self, phone_number, password=None, **extra_fields):
-        """
-        Creates and saves a superuser with the given phone_number and password.
-        """
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        
-        user = self.create_user(
-            phone_number=phone_number,
-            password=password,
-            **extra_fields
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+
+        if extra_fields.get("is_staff") is not True:
+            msg = "Superuser must have is_staff=True."
+            raise ValueError(msg)
+        if extra_fields.get("is_superuser") is not True:
+            msg = "Superuser must have is_superuser=True."
+            raise ValueError(msg)
+
+        return self._create_user(phone_number, password, **extra_fields)
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.forms",
     # apps
     "apps.users",
     # third_party packages
